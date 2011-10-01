247aa8c0967f1a75fdd2e2027f6ea5aaaffde0e4
Laurent Claessens <moky.math@gmail.com>
2011-10-01 19:18:10 +0200

    modified:   espaces_mesure.tex 	new file:   phystricksMaxVraissLp.py 	modified:   statistiques.tex

---
+++ phystricksMaxVraissLp.py
@@ -0,0 +1,21 @@
+from phystricks import *
+def MaxVraissLp():
+    pspict,fig = SinglePicture("MaxVraissLp")
+
+    x=var('x')
+    f=phyFunction(binomial(10,3)*x**3*(1-x)**7).graph(0,1)
+
+    M=f.get_point(0.3)
+    Mx=M.projection(pspict.axes.single_axeX)
+    l=Segment(M,Mx)
+    l.parameters.style="dotted"
+
+    pspict.axes.single_axeX.add_label(0.3,-90,"$p$")
+    pspict.axes.single_axeX.Dx=0.3
+    pspict.axes.single_axeY.Dx=0.2
+
+    pspict.DrawGraphs(M,f,l)
+    pspict.DrawDefaultAxes()
+    pspict.dilatation(10)
+    fig.conclude()
+    fig.write_the_file()
