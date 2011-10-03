2940df72967269c6d73ad61323e7acea3ec764fd
Laurent Claessens <moky.math@gmail.com>
2011-10-03 21:20:58 +0200

    modified:   figures_modelisation.py 	modified:   phystricksBiaisOuPas.py

--- phystricksBiaisOuPas.py
+++ phystricksBiaisOuPas.py
@@ -7,8 +7,12 @@ def BiaisOuPas():
     def normale(m,s):
         return phyFunction( 1/(s*sqrt(2*pi))*exp(-((x-m)/2)**2) )
 
+    f1=normale(0,2)
+    f2=normale(-0.2,1)
 
-    pspict.DrawGraphs(<++>)
+    f2.parameters.color="red"
+
+    pspict.DrawGraphs(f1,f2)
     pspict.DrawDefaultAxes()
     pspict.dilatation(1)
     fig.conclude()
