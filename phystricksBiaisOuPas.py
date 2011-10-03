d3f0eb67ea9c028f1c6ed1fbdd56b21e22afa9d5
Laurent Claessens <moky.math@gmail.com>
2011-10-03 22:02:18 +0200

    modified:   phystricksBiaisOuPas.py 	modified:   statistiques.tex

--- phystricksBiaisOuPas.py
+++ phystricksBiaisOuPas.py
@@ -5,15 +5,23 @@ def BiaisOuPas():
     
     x=var('x')
     def normale(m,s):
-        return phyFunction( 1/(s*sqrt(2*pi))*exp(-((x-m)/2)**2) )
+        return phyFunction( 1/(s*sqrt(2*pi))*exp(-((x-m)/s)**2) ).graph(-3,3)
 
-    f1=normale(0,2)
-    f2=normale(-0.2,1)
+    f1=normale(0,1)
+    f2=normale(-0.2,0.2)
+
+    f2.plotpoints=1000
+
+    a=0.5
+    I=Segment(Point(-a,0),Point(a,0))
+    measure=MeasureLength(I,0.2)
+    measure.put_mark(0.3,measure.advised_mark_angle,"\( I\)")
+    measure.parameters.color="cyan"
 
     f2.parameters.color="red"
 
-    pspict.DrawGraphs(f1,f2)
+    pspict.DrawGraphs(f1,f2,measure)
     pspict.DrawDefaultAxes()
-    pspict.dilatation(1)
+    pspict.dilatation(3)
     fig.conclude()
     fig.write_the_file()
