f4e82ee872cac8f8105b35b648ccb098fd74df72
Laurent Claessens <moky.math@gmail.com>
2011-10-20 13:08:20 +0200

    Markov

---
+++ lst_markov.py
@@ -0,0 +1,16 @@
+#! /usr/bin/python
+# -*- coding: utf8 -*-
+
+import LaTeXparser
+import LaTeXparser.PytexTools
+
+def Prerequiste(medicament):
+	medicament.Compil.verif=True
+
+myRequest = LaTeXparser.PytexTools.Request("markov")
+myRequest.prerequiste_list.append(Prerequiste)
+myRequest.original_filename="modelisation.tex"
+
+myRequest.ok_filenames_list=["e_modelisation"]
+myRequest.ok_filenames_list.append("espaces_mesure")
+myRequest.ok_filenames_list.append("Markov")
