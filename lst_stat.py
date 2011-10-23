5b640ae802491dfcbb117b1b13dfc4358d897c63
Laurent Claessens <moky.math@gmail.com>
2011-10-23 08:00:08 +0200

    Théorème de Cochran

---
+++ lst_stat.py
@@ -0,0 +1,17 @@
+#! /usr/bin/python
+# -*- coding: utf8 -*-
+
+import LaTeXparser
+import LaTeXparser.PytexTools
+
+def Prerequiste(medicament):
+	medicament.Compil.verif=True
+
+myRequest = LaTeXparser.PytexTools.Request("statistiques")
+myRequest.prerequiste_list.append(Prerequiste)
+myRequest.original_filename="modelisation.tex"
+
+myRequest.ok_filenames_list=["e_modelisation"]
+myRequest.ok_filenames_list.append("espaces_mesure")
+myRequest.ok_filenames_list.append("lois_usuelles")
+myRequest.ok_filenames_list.append("statistiques")
