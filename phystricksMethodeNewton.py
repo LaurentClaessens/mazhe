c801d097ae63d62ff360cd92aa54ae4f1c84f4bc
Laurent Claessens <moky.math@gmail.com>
2011-11-26 11:27:26 +0100

    Gérer les <++>

--- phystricksMethodeNewton.py
+++ phystricksMethodeNewton.py
@@ -1,7 +1,8 @@
 from phystricks import *
+import phystricks.MathConstructions
+
 def MethodeNewton():
     pspict,fig = SinglePicture("MethodeNewton")
-
 
     x=var('x')
     a = -1.5
@@ -11,31 +12,31 @@ def MethodeNewton():
     xn = -0.3
     mx = -0.5
     Mx = 2
-    F=Graph(f,mx,Mx)
+    F=f.graph(mx,Mx)
     pspict.DrawGraph(F)
 
     newton = phystricks.MathConstructions.NewtonMethod(f)
     elements = newton.step_from_point(xn)
-    A=Graph(elements.A)
-    B=Graph(elements.B)
-    P=Graph(elements.P)
-    A.mark(0.3,-90,"$x_n$")
-    B.mark(0.3,-90,"$x_{n+1}$")
-    P.mark(0.3,45,"$y_n$")
-    pspict.DrawSegment(elements.vertical_segment,"linestyle=dotted,linecolor=red")
-    pspict.DrawSegment(elements.diagonal_segment,"linestyle=dashed,linecolor=green")
-    pspict.DrawGraph(A)
-    pspict.DrawGraph(B)
-    pspict.DrawGraph(P)
+    A=elements.A
+    B=elements.B
+    P=elements.P
+    A.put_mark(0.3,-90,"$x_n$")
+    B.put_mark(0.3,-90,"$x_{n+1}$")
+    P.put_mark(0.3,45,"$y_n$")
+    elements.vertical_segment.parameters.style="dotted"
+    elements.vertical_segment.parameters.color="red"
+    elements.diagonal_segment.parameters.style="dashed"
+    elements.diagonal_segment.parameters.color="green"
+    pspict.DrawGraphs(elements.vertical_segment,elements.diagonal_segment,A,B,P)
 
     roots = f.roots()
     for i in range(len(roots)):
-        P = Graph(roots[i])
-        P.mark(0.3,90,"$r_%s$"%str(i))
+        P = roots[i]
+        P.put_mark(0.3,90,"$r_%s$"%str(i))
         pspict.DrawGraph(P)
     sommet=f.get_point( (roots[0].x+roots[1].x)/2 )
-    C=Graph(sommet)
-    C.mark(0.3,-90,"$S$")
+    C=sommet
+    C.put_mark(0.3,-90,"$S$")
     pspict.DrawGraph(C)
 
 
