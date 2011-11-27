7a1a06ac02a7157be106bde43957414c24f05260
Laurent Claessens <moky.math@gmail.com>
2011-11-27 12:18:35 +0100

    Proba conditionnelles

--- lst_markov.py
+++ lst_markov.py
@@ -9,8 +9,7 @@ def Prerequiste(medicament):
 
 myRequest = LaTeXparser.PytexTools.Request("markov")
 myRequest.prerequiste_list.append(Prerequiste)
-myRequest.original_filename="modelisation.tex"
+myRequest.original_filename="mes_notes.tex"
 
-myRequest.ok_filenames_list=["e_modelisation"]
-#myRequest.ok_filenames_list.append("espaces_mesure")
+myRequest.ok_filenames_list=["e_mes_notes"]
 myRequest.ok_filenames_list.append("Markov")
