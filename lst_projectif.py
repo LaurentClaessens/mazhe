ca55c9b698a761aaebae1bb6b5843fee461b06ff
Laurent Claessens <moky.math@gmail.com>
2011-12-30 09:49:54 +0100

    espaces projectifs

---
+++ lst_projectif.py
@@ -0,0 +1,15 @@
+#! /usr/bin/python
+# -*- coding: utf8 -*-
+
+import LaTeXparser
+import LaTeXparser.PytexTools
+
+def Prerequiste(medicament):
+	medicament.Compil.verif=True
+
+myRequest = LaTeXparser.PytexTools.Request("projectifs")
+myRequest.prerequiste_list.append(Prerequiste)
+myRequest.original_filename="mes_notes.tex"
+
+myRequest.ok_filenames_list=["e_mes_notes"]
+myRequest.ok_filenames_list.append("EspacesProjectifs")
