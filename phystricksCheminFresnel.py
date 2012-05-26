8f67df1dcc4496284be0095c6a9e4d357770b94b
Laurent Claessens <moky.math@gmail.com>
2012-05-26 19:50:25 +0200

    modified:   AnalyseComplexe.tex 	modified:   phystricksCheminFresnel.py

--- phystricksCheminFresnel.py
+++ phystricksCheminFresnel.py
@@ -9,13 +9,30 @@ def CheminFresnel():
     gamma2=PolarCurve(R).graph(0,pi/4)
     gamma3=ParametricCurve(x*cos(pi/4),x*sin(pi/4)).graph(0,R)
 
+    gamma1.put_arrow(R/2)
+    gamma2.put_arrow(pi/8)
+    gamma3.put_arrow(R/2)
+
+    P1=gamma1.get_point(R/2)
+    P2=gamma2.get_point(pi/8)
+    P3=gamma3.get_point(R/2)
+
+    P1.parameters.symbol="none"
+    P2.parameters.symbol="none"
+    P3.parameters.symbol="none"
+
+    P1.put_mark(0.1,P1.advised_mark_angle,"\( \gamma_1\)",automatic_place=pspict)
+    P2.put_mark(0.1,P2.advised_mark_angle,"\( \gamma_2\)",automatic_place=pspict)
+    P3.put_mark(0.1,P3.advised_mark_angle+180,"\( \gamma_3\)",automatic_place=pspict)
+
     gamma1.parameters.color="blue"
     gamma2.parameters.color=gamma1.parameters.color
     gamma3.parameters.color=gamma1.parameters.color
     
 
-    pspict.DrawGraphs(gamma1,gamma2,gamma3)
+    pspict.DrawGraphs(gamma1,gamma2,gamma3,P1,P2,P3)
     pspict.axes.no_numbering()
+    pspict.axes.no_graduation()
     pspict.DrawDefaultAxes()
     fig.conclude()
     fig.write_the_file()
