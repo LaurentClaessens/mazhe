b3fd1bccb1b38052405e401277729ab6c8019dec
Laurent Claessens <moky.math@gmail.com>
2012-07-30 19:54:16 +0200

    un peu d'ordre

--- lst_markov.py
+++ lst_markov.py
@@ -4,11 +4,8 @@
 import LaTeXparser
 import LaTeXparser.PytexTools
 
-def Prerequiste(medicament):
-	medicament.Compil.verif=True
 
 myRequest = LaTeXparser.PytexTools.Request("markov")
-myRequest.prerequiste_list.append(Prerequiste)
 myRequest.original_filename="mes_notes.tex"
 
 myRequest.ok_filenames_list=["e_mes_notes"]
--- lst_stat.py
+++ lst_stat.py
@@ -4,15 +4,12 @@
 import LaTeXparser
 import LaTeXparser.PytexTools
 
-def Prerequiste(medicament):
-	medicament.Compil.verif=True
 
 myRequest = LaTeXparser.PytexTools.Request("statistiques")
-myRequest.prerequiste_list.append(Prerequiste)
 myRequest.original_filename="mes_notes.tex"
 
 myRequest.ok_filenames_list=["e_mes_notes"]
-#myRequest.ok_filenames_list.append("espaces_mesure")
+myRequest.ok_filenames_list.append("espaces_mesure")
 myRequest.ok_filenames_list.append("lois_usuelles")
 myRequest.ok_filenames_list.append("statistiques")
 myRequest.ok_filenames_list.append("vars_al_proba")
