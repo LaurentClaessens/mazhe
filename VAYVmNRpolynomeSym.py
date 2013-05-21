522647e980a85902e6922c5c7069f677f5ba49a8
Laurent Claessens <moky.math@gmail.com>
2013-05-21 14:36:58 +0000

    Début du théorème de Kronecker.

--- VAYVmNRpolynomeSym.py
+++ VAYVmNRpolynomeSym.py
@@ -1,5 +1,4 @@
 sage: P(x)=x**3+2*x**2+3*x+4
-sage: sols=solve(  P(x)==0,x  )                                                                                                            
 sage: S=solve(  P(x)==0,x  )   
 sage: sols=[ s.rhs() for s in S  ]
 sage: Q=[  s**2 for s in sols ] 
