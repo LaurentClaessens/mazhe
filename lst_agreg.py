2a319ce9b5d99c6fd9a0186216478bb872fe1698
Laurent Claessens <moky.math@gmail.com>
2013-12-22 15:43:53 +0000

    Semble accepter la compilation pour l'agrégation.

--- lst_agreg.py
+++ lst_agreg.py
@@ -10,4 +10,4 @@ import plugins_agreg
 myRequest = LaTeXparser.PytexTools.Request("mesure")
 myRequest.medicament_plugin=plugins_agreg.accept_all_input
 myRequest.plugin_list=[plugins_agreg.set_isAgreg,plugins_agreg.remove_parts]
-myRequest.original_filename="mes_notes.tex"
+myRequest.original_filename="mazhe.tex"
