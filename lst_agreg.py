14f00ebeee8daaae963399fe8447c062546d3860
Laurent Claessens <moky.math@gmail.com>
2014-02-10 17:58:28 +0000

    Un peu de remise en ordre entre limite et continuité.

--- lst_agreg.py
+++ lst_agreg.py
@@ -5,9 +5,12 @@ from __future__ import unicode_literals
 
 import LaTeXparser
 import LaTeXparser.PytexTools
+import commons
 import plugins_agreg
 
 myRequest = LaTeXparser.PytexTools.Request("mesure")
+myRequest.ok_hash=commons.ok_hash
+
 myRequest.medicament_plugin=plugins_agreg.accept_all_input
 myRequest.plugin_list=[plugins_agreg.set_isAgreg,plugins_agreg.remove_parts]
 myRequest.original_filename="mazhe.tex"
