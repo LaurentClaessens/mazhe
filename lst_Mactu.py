5a68d2e3b0fdd43b4a4bff25bf7fe42010d73b2b
Laurent Claessens <moky.math@gmail.com>
2014-06-13 13:11:00 +0200

    Un peu d'ordre.

--- lst_Mactu.py
+++ lst_Mactu.py
@@ -6,8 +6,27 @@ from __future__ import unicode_literals
 import LaTeXparser
 import LaTeXparser.PytexTools
 import commons
+import plugins_agreg
 
 myRequest = LaTeXparser.PytexTools.Request("futur")
+
+
+mazhe_mark_list=[]
+mazhe_mark_list.append("% SCRIPT MARK -- DECLARATIVE PART")
+mazhe_mark_list.append("% SCRIPT MARK -- GARDE MAZHE")
+mazhe_mark_list.append("% SCRIPT MARK -- TOC")
+mazhe_mark_list.append("% SCRIPT MARK -- ENGLISH INTRODUCTION")
+mazhe_mark_list.append("% SCRIPT MARK -- INTRO SAGE")
+mazhe_mark_list.append("% SCRIPT MARK -- AGRÉGATION")
+mazhe_mark_list.append("% SCRIPT MARK -- OUTILS MATHÉMATIQUE")
+mazhe_mark_list.append("% SCRIPT MARK -- RESEARCH PART")
+mazhe_mark_list.append("% SCRIPT MARK -- MATLAB")
+mazhe_mark_list.append("% SCRIPT MARK -- EXERCICES")
+mazhe_mark_list.append("% SCRIPT MARK -- FINAL")
+
+
+myRequest.plugin_list=[plugins_agreg.keep_script_marks(mazhe_mark_list)]
+
 myRequest.original_filename="mazhe.tex"
 
 myRequest.ok_hash=commons.ok_hash
