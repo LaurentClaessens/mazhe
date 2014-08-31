1729b02072d4a4ee90870426c19b2048504f2763
Laurent Claessens <moky.math@gmail.com>
2014-08-31 13:49:53 +0200

    liste des fichiers nonagreg

--- lst_nonagreg.py
+++ lst_nonagreg.py
@@ -18,9 +18,6 @@ myRequest.ok_hash=commons.ok_hash
 # L'ordre dans les plugin est important parce que set_isAgreg retourne un code latex sans les commentaires
 # alors que keep_script_marks compte dessus pour faire sa sélection.
 myRequest.add_plugin(plugins_agreg.accept_all_input,"medicament")
-myRequest.add_plugin(plugins_agreg.keep_script_marks(plugins_agreg.agreg_mark_list),"after_pytex")
-myRequest.add_plugin(plugins_agreg.set_isAgreg,"after_pytex")
-myRequest.add_plugin(plugins_agreg.set_commit_hexsha,"after_pytex")
-myRequest.add_plugin(plugins_agreg.ultimate_git,"before_compilation")
+myRequest.add_plugin(plugins_agreg.keep_script_marks(plugins_nonagreg.agreg_mark_list),"after_pytex")
 
 myRequest.original_filename="mazhe.tex"
