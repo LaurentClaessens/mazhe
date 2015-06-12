f996a55e71b9f361410c974b606736c8ecdc8cea
Laurent Claessens <moky.math@gmail.com>
2015-06-12 17:52:40 +0200

    Deux autres ficjiers.

---
+++ lst_outils_math.py
@@ -0,0 +1,22 @@
+#! /usr/bin/python
+# -*- coding: utf8 -*-
+
+from __future__ import unicode_literals
+
+import LaTeXparser
+import LaTeXparser.PytexTools
+import commons
+import plugins_agreg
+
+myRequest = LaTeXparser.PytexTools.Request("mesure")
+myRequest.ok_hash=commons.ok_hash
+
+# L'ordre dans les plugin est important parce que set_isAgreg retourne un code latex sans les commentaires
+# alors que keep_script_marks compte dessus pour faire sa sélection.
+myRequest.add_plugin(LaTeXparser.PytexTools.accept_all_input,"medicament")
+myRequest.add_plugin(LaTeXparser.PytexTools.keep_script_marks(plugins_agreg.outilsmath_mark_list),"before_pytex")
+myRequest.add_plugin(plugins_agreg.set_isAgreg,"before_pytex")
+
+myRequest.add_plugin(plugins_agreg.set_filename("0-outils_math.pdf"),"medicament")
+
+myRequest.original_filename="mazhe.tex"
