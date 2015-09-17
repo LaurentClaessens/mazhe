331b849dc0e451ec050e5c758c791afff7807f57
Laurent Claessens <moky.math@gmail.com>
2015-09-17 15:33:30 +0200

    On avance dans le programme qui va diviser le Frido en plusieurs volumes.

--- volumes.cpp
+++ volumes.cpp
@@ -20,6 +20,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <cstdlib>
+#include <string>
 using namespace std;
 
 
@@ -45,35 +47,71 @@ class IeC_couple{
         replacement=r;
     };
     void perform_substitution(string &str){
-        cout<<"replacer|"<<code<<"| par "<<replacement<<endl;
         replace(str,code,replacement);
     };
 };
 
 // replace "\IeC {\'e}" by "é" and other such replacements.
 void IeC_remove(string &line){
-    IeC_couple icirconflex("\\^\\i ","î");
-    IeC_couple eaccute("\\'e","é");
-    vector<IeC_couple> change_list(2)
-    change_list.push_back(icirconflex)
-    change_list.push_back(eaccute)
+    vector<IeC_couple> change_list;
+    change_list.push_back(IeC_couple("\\^\\i ","î") );
+    change_list.push_back(IeC_couple("\\'e","é"));
+    change_list.push_back(IeC_couple("\\^e","ê"));
+    change_list.push_back(IeC_couple("\\^o","ô"));
+    change_list.push_back(IeC_couple("\\\"o","ö"));
+    change_list.push_back(IeC_couple("\\\"u","ü"));
+    change_list.push_back(IeC_couple("\\`e","è")  );
+    change_list.push_back(IeC_couple("\\`A","Á")  );
+    change_list.push_back(IeC_couple("\\`u","ù")  );
+    change_list.push_back(IeC_couple("\\`a","à")  );
+    change_list.push_back(IeC_couple("\\'E","É")  );
 
+    for (int i=0;i!=change_list.size();i++){
+        change_list[i].perform_substitution(line);
+    };
+};
 
-    eaccute.perform_substitution(line);
-    cout<<line<<endl;
+bool is_chapter_line(string line){
+        string chapter_mark="\\contentsline {chapter}{\\numberline {";
+        int is=chapter_mark.size();
+        string deb(line,0,is);
+        if (deb==chapter_mark){return true;}
+        return false;
+};
 
+// We parse a line like that :
+// \contentsline {chapter}{\numberline {30}Exemples avec Sage}{1284}{chapter.30}
+// There can be braces in the chapter title. 
+class chapter{
+    public:
+        string name;
+        int page;
 
+    chapter(string line){
+        size_t lbrace=line.rfind("{");
+        size_t cbrace=line.rfind("{",lbrace-1);
+        string spage=line.substr(cbrace+1,lbrace-cbrace-2);
+        page=atoi(spage.c_str());
+        size_t fbrace=line.find("}");
+        size_t init=line.find("}",fbrace+1)+1;
+        name= line.substr(init,cbrace-init-1);
+    };
 };
 
 int main ()
 {
     ifstream tocfile("test.toc");
     string line;
-    getline(tocfile,line);
-    tocfile.close();
 
-    
-    cout<<line<<endl;
-    IeC_remove(line);
-    cout<<"le résultat final est "<<endl<< line<<endl;
+    while(!tocfile.eof()){
+        getline(tocfile,line);
+        if (is_chapter_line(line)){
+            IeC_remove(line);
+            cout<<line<<endl;
+            chapter c=chapter(line);
+            cout<<c.page<<endl;
+            cout<<c.name<<endl;
+        };
+    };
+    tocfile.close();
 };
