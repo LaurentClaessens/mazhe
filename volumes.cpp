b7dc1cf37afdf8f87e7cd706be90c09575fc78d9
Laurent Claessens <moky.math@gmail.com>
2015-09-18 18:26:39 +0200

    soir

--- volumes.cpp
+++ volumes.cpp
@@ -34,7 +34,7 @@ void replace( string &str,string s1,string s2 ){
         str.replace(  position,s1.length(),s2 );
         position=str.find(s1);
     };
-};
+}
 
 // Provide a couple like "\IeC {\^\i }" stands for "î".
 // You only gives what is in the bracket.
@@ -69,18 +69,20 @@ void IeC_remove(string &line){
     for (int i=0;i!=change_list.size();i++){
         change_list[i].perform_substitution(line);
     };
-};
+}
 
 bool is_chapter_line(string line){
-        string chapter_mark="\\contentsline {chapter}{\\numberline {";
+        string chapter_mark="\\contentsline {chapter}";
         int is=chapter_mark.size();
         string deb(line,0,is);
         if (deb==chapter_mark){return true;}
         return false;
-};
+}
 
 // We parse a line like that :
 // \contentsline {chapter}{\numberline {30}Exemples avec Sage}{1284}{chapter.30}
+// or :
+// \contentsline {chapter}{Index}{7}{chapter*.1}
 // There can be braces in the chapter title. 
 class chapter{
     public:
@@ -88,30 +90,69 @@ class chapter{
         int page;
 
     chapter(string line){
-        size_t lbrace=line.rfind("{");
-        size_t cbrace=line.rfind("{",lbrace-1);
-        string spage=line.substr(cbrace+1,lbrace-cbrace-2);
+        string a1=line;
+        replace(a1,"\\contentsline {chapter}{","");
+        size_t initpos=0;
+        if(a1[0]==*"\\"){ initpos=a1.find("}")+1 ;};
+        a1.replace(0,initpos,"");
+
+        size_t lbrace=a1.rfind("{");
+        size_t cbrace=a1.rfind("{",lbrace-1);
+
+        string spage=a1.substr(cbrace+1,lbrace-cbrace-2);
         page=atoi(spage.c_str());
-        size_t fbrace=line.find("}");
-        size_t init=line.find("}",fbrace+1)+1;
-        name= line.substr(init,cbrace-init-1);
+
+        name= a1.substr(0,cbrace-1);
+    };
+};
+
+// Describe a LaTeX document. One has to provide coherent pdf filename and a tof filename that are produced by LaTeX.
+class latex_document{
+    public:
+        string pdf_filename;
+        vector<chapter> chapter_list;
+
+    latex_document(string pdf, string toc){
+        pdf_filename=pdf;
+
+        ifstream tocfile(toc.c_str());
+        string line;
+
+        while(!tocfile.eof()){
+            getline(tocfile,line);
+            if (is_chapter_line(line)){
+                IeC_remove(line);
+                chapter_list.push_back(chapter(line));
+            };
+        };
+        tocfile.close();
+    }
+    // create a pdf file 'output' containing the chapters from 'Ichap' to 'Fchap' (the latter is not included).
+    void extract_from_to(string Ichap,string Fchap,string output){
+        bool If=false;
+        bool Ff=false;
+        int Ipage=0;
+        int Fpage=0;
+        for (int i=0; !If or !Ff ;i++){
+            chapter chap=chapter_list[i];
+            if (chap.name==Ichap){
+                Ipage=chap.page;
+                If=true;
+            }
+            if (chap.name==Fchap){
+                Fpage=chap.page-1;
+                Ff=true;
+            }
+        }
+        cout<<Ipage<<" "<<Fpage<<endl;
+        string command="";
+        command<<"pdftk "<<pdf_filename<<" cat ";
+        cout<<command<<endl;
     };
 };
 
 int main ()
 {
-    ifstream tocfile("test.toc");
-    string line;
-
-    while(!tocfile.eof()){
-        getline(tocfile,line);
-        if (is_chapter_line(line)){
-            IeC_remove(line);
-            cout<<line<<endl;
-            chapter c=chapter(line);
-            cout<<c.page<<endl;
-            cout<<c.name<<endl;
-        };
-    };
-    tocfile.close();
-};
+    latex_document frido("0-lefrido.pdf","Inter_frido-mazhe_pytex.toc");
+    frido.extract_from_to("Retour sur les groupes","Chaînes de Markov à temps discret","mat2.pdf");
+}
