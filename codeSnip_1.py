7381d0402f889fb1464dbae3de2b043b2f9b4393
Laurent Claessens <moky.math@gmail.com>
2016-04-01 13:53:36 +0200

    test de convention de codage dans codeSnip_1.py

--- codeSnip_1.py
+++ codeSnip_1.py
@@ -1,6 +1,6 @@
 from __future__ import division
 
-def bissection(f,a,b,toll,mmax):
+def bissection(f, a, b, toll, mmax):
     """
     f : une fonction
     a,b : les limites de l'intervalle
@@ -11,16 +11,16 @@ def bissection(f,a,b,toll,mmax):
 
     Retourne un tuple (x,n) où 'x' est la solution approchée et 'n' est le nombre d'itérations effefctuées
     """
-    n=-1
-    amp=toll+1 # Pour s'assurer que l'on entre dans le cycle
-    while amp>toll and n<nmax :
-        n=n+1
-        amp=abs(b-a)
-        x=a+amp/2
-        if f(a)*f(x)<0:
-            b=x
-        elif f(a)*f(x)>0:
-            a=x
+    n = -1
+    amp = toll + 1 # Pour s'assurer que l'on entre dans le cycle
+    while amp > toll and n < nmax :
+        n = n + 1
+        amp=abs(b - a)
+        x = a + amp / 2
+        if f(a) * f(x) < 0:
+            b = x
+        elif f(a)*f(x) > 0:
+            a = x
         else :      # Problème ZERO
-            amp=0
-    return (x,n)
+            amp = 0
+    return (x, n)
