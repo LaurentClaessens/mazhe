3b3c863829de4aad7d45da7bb07993ab394b77e9
Laurent Claessens <moky.math@gmail.com>
2016-10-02 18:57:59 +0200

    Les sources des figures sont dans mazhe/pictures_src Les fichers tex des figures sont dans mazhe/pictures_tex
    Il a fallu changer tous les \input des figures un peu partout.
    
    https://github.com/LaurentClaessens/mazhe/issues/14#issuecomment-250981443

---
+++ pictures_src/Directories.py
@@ -0,0 +1,11 @@
+
+# MAIN_TEX 
+#   is the directory of the main latex file.
+
+# PICTURES_TEX
+#   is the directory in which the latex code of the picture will be written.
+
+# Directories are given relative to the one where the source pictures are.
+
+MAIN_TEX=".."
+PICTURES_TEX="../pictures_tex"
