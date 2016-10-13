bed951638670a7077654a95726b384d79590c2c3
Laurent Claessens <moky.math@gmail.com>
2016-10-13 03:47:14 +0200

    Un pour travailler.

---
+++ lst_actu.py
@@ -0,0 +1,26 @@
+#! /usr/bin/python
+# -*- coding: utf8 -*-
+
+from __future__ import unicode_literals
+
+import latexparser
+import latexparser.PytexTools
+import commons
+import plugins_agreg
+
+myRequest = latexparser.PytexTools.Request()
+myRequest.ok_hash=commons.ok_hash
+
+myRequest.add_plugin(plugins_agreg.set_isFrido,"before_pytex")
+
+myRequest.original_filename="mazhe.tex"
+
+myRequest.ok_filenames_list=["e_mazhe"]
+myRequest.ok_filenames_list.extend(["101_mazhe"])
+myRequest.ok_filenames_list.extend(["<++>"])
+myRequest.ok_filenames_list.extend(["<++>"])
+myRequest.ok_filenames_list.extend(["<++>"])
+myRequest.ok_filenames_list.extend(["<++>"])
+
+
+myRequest.new_output_filename="0-actu.pdf"
