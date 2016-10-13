a41259c8b6c52dfdf2c4f16b7106e4cbe3addd36
Laurent Claessens <moky.math@gmail.com>
2016-10-13 16:09:35 +0200

    Démonstration des théorèmes de Fubini et de ses copains (Tonelli).
    Prochaine étape : ce qu'il se passe lorsque les fonctions sont à valeurs
    complexes. Nous en aurons besoin pour inverser des intégrales dans la
    partie sur les complexes (84_AnalyseComplexe)

--- lst_actu.py
+++ lst_actu.py
@@ -16,11 +16,11 @@ myRequest.add_plugin(plugins_agreg.set_isFrido,"before_pytex")
 myRequest.original_filename="mazhe.tex"
 
 myRequest.ok_filenames_list=["e_mazhe"]
-myRequest.ok_filenames_list.extend(["101_mazhe"])
+myRequest.ok_filenames_list.extend(["84_AnalyseComplexe"])
+myRequest.ok_filenames_list.extend(["71_suites_series_fonctions"])
+myRequest.ok_filenames_list.extend(["134_choses_finales"])
 myRequest.ok_filenames_list.extend(["<++>"])
-myRequest.ok_filenames_list.extend(["<++>"])
-myRequest.ok_filenames_list.extend(["<++>"])
-myRequest.ok_filenames_list.extend(["<++>"])
+myRequest.ok_filenames_list.extend(["157_thematique"])
 
 
 myRequest.new_output_filename="0-actu.pdf"
