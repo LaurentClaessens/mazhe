ab63377e36207c3e42eebb0e4105a6e0edeece17
Laurent Claessens <moky.math@gmail.com>
2016-10-17 14:48:16 +0200

    Deux distances sur N qui donnent des espaces topologiques isomorphes, mais des notions de suites de Cauchy différentes. L'un est complet, l'autre pas.

--- lst_actu.py
+++ lst_actu.py
@@ -16,15 +16,10 @@ myRequest.add_plugin(plugins_agreg.set_isFrido,"before_pytex")
 myRequest.original_filename="mazhe.tex"
 
 myRequest.ok_filenames_list=["e_mazhe"]
-myRequest.ok_filenames_list.extend(["84_AnalyseComplexe"])
-myRequest.ok_filenames_list.extend(["71_suites_series_fonctions"])
 myRequest.ok_filenames_list.extend(["134_choses_finales"])
+myRequest.ok_filenames_list.extend(["53_topologie"])
+myRequest.ok_filenames_list.extend(["168_questionsFrido"])
 myRequest.ok_filenames_list.extend(["<++>"])
-myRequest.ok_filenames_list.extend(["<++>"])
-myRequest.ok_filenames_list.extend(["<++>"])
-myRequest.ok_filenames_list.extend(["<++>"])
-myRequest.ok_filenames_list.extend(["<++>"])
-myRequest.ok_filenames_list.extend(["73_Chap_integrales_multiples"])
 myRequest.ok_filenames_list.extend(["157_thematique"])
 
 
