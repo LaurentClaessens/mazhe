73caba5e43a76dfed6e32df28b2cd590d92eb067
Laurent Claessens <moky.math@gmail.com>
2016-11-12 08:29:17 +0100

    Birapport : définition et premières propriétés.

--- lst_actu.py
+++ lst_actu.py
@@ -17,8 +17,6 @@ myRequest.original_filename="mazhe.tex"
 
 myRequest.ok_filenames_list=["e_mazhe"]
 myRequest.ok_filenames_list.extend(["161_EspacesProjectifs"])
-myRequest.ok_filenames_list.extend(["160_hyperbolique"])
-myRequest.ok_filenames_list.extend(["55_espace_vecto_norme"])
 myRequest.ok_filenames_list.extend(["<++>"])
 myRequest.ok_filenames_list.extend(["<++>"])
 myRequest.ok_filenames_list.extend(["<++>"])
