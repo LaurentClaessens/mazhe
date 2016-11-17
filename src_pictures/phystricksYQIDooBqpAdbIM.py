a002da761d62ecd9fa015497393b6daf9ade512a
Laurent Claessens <moky.math@gmail.com>
2016-11-17 10:19:57 +0100

    Théorème de l'angle inscrit et cocyclicité.

--- src_pictures/phystricksYQIDooBqpAdbIM.py
+++ src_pictures/phystricksYQIDooBqpAdbIM.py
@@ -7,15 +7,28 @@ def YQIDooBqpAdbIM():
     pspict.dilatation(1)
 
     A=Point(0,0)
-    B=Point(3,0)
-    O=Segment(A,B).midpoint()+(0,4)
+    B=Point(5,0)
+    O=Segment(A,B).midpoint()+(0,3)
 
     trig=Polygon(A,B,O)
     trig.put_mark(0.4,points_names="ABO",pspict=pspict)
 
-    a1=AngleAOB(B,A,O)
+    aA=AngleAOB(B,A,O)
+    aA.parameters.color="red"
+    aA.put_arrow(pspict=pspict)
 
-    pspict.DrawGraphs(trig,a1)
+    aO=AngleAOB(A,O,B)
+    aO.parameters.color="red"
+    aO.put_arrow(pspict=pspict)
+
+    aB=AngleAOB(O,B,A)
+    aB.parameters.color="red"
+    aB.put_arrow(pspict=pspict)
+
+    pspict.DrawGraphs(trig,aA,aO,aB)
+
+    pspict.comment="The angles are red and an arrow is drawn in the trigonometric sense."
+
     fig.no_figure()
     fig.conclude()
     fig.write_the_file()
