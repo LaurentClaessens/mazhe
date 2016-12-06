963a0fa7b0c3d49958458040263c1a65fb804571
Laurent Claessens <laurent@claessens-donadello.eu>
2016-12-06 10:10:40 +0100

    - Les couvertures et le script de division du Frido en volumes.

---
+++ publication/volumes/decoupe.sh
@@ -0,0 +1,6 @@
+#Mini script qui lance la compilation du Frido dans sa version à commercialiser et qui génère les trois volumes "corrects".
+
+# Attention : ce fichier doit être dans le répertoire des sources LaTeX.
+
+pytex lst_book.py
+./volumes/volumes
