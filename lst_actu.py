5a06062f28ecd2ced1c5e2e9a81bf8a70065e4ac
Laurent Claessens <laurent@claessens-donadello.eu>
2016-12-20 09:34:58 +0100

    Petite précision quant à la méthode du gradient à pas optimal. Si la méthode s'arrête, c'est qu'on a trouvé la solution. Ça a l'air bête, mais il faut le préciser.

--- lst_actu.py
+++ lst_actu.py
@@ -17,8 +17,7 @@ myRequest.original_filename="mazhe.tex"
 
 myRequest.ok_filenames_list=["e_mazhe"]
 myRequest.ok_filenames_list.extend(["84_AnalyseComplexe"])
-myRequest.ok_filenames_list.extend(["<++>"])
-myRequest.ok_filenames_list.extend(["<++>"])
+myRequest.ok_filenames_list.extend(["79_inversion_locale"])
 myRequest.ok_filenames_list.extend(["<++>"])
 myRequest.ok_filenames_list.extend(["<++>"])
 myRequest.ok_filenames_list.extend(["<++>"])
