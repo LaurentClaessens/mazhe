e92bab08fd649a260613d7b0431c48ee1a0c89da
Laurent Claessens <laurent@claessens-donadello.eu>
2016-12-28 05:13:18 +0100

    Un peu d'ordre Rolle et accroissements finis.

--- lst_actu.py
+++ lst_actu.py
@@ -16,9 +16,7 @@ myRequest.add_plugin(plugins_agreg.set_isFrido,"before_pytex")
 myRequest.original_filename="mazhe.tex"
 
 myRequest.ok_filenames_list=["e_mazhe"]
-myRequest.ok_filenames_list.extend(["78_inversion_locale"])
-myRequest.ok_filenames_list.extend(["89_Equations_diff"])
-myRequest.ok_filenames_list.extend(["<++>"])
+myRequest.ok_filenames_list.extend(["66_Chap_calcul_differentiel"])
 myRequest.ok_filenames_list.extend(["<++>"])
 myRequest.ok_filenames_list.extend(["<++>"])
 myRequest.ok_filenames_list.extend(["<++>"])
