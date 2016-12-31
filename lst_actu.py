630e8552cc8cb1f38ce2053e661aa87c652425e7
Laurent Claessens <laurent@claessens-donadello.eu>
2016-12-31 15:59:11 +0100

    Définition de sinus et de cosinus par les séries. Premières propriétés.

--- lst_actu.py
+++ lst_actu.py
@@ -16,9 +16,7 @@ myRequest.add_plugin(plugins_agreg.set_isFrido,"before_pytex")
 myRequest.original_filename="mazhe.tex"
 
 myRequest.ok_filenames_list=["e_mazhe"]
-myRequest.ok_filenames_list.extend(["66_Chap_calcul_differentiel"])
-myRequest.ok_filenames_list.extend(["53_topologie"])
-myRequest.ok_filenames_list.extend(["<++>"])
+myRequest.ok_filenames_list.extend(["75_series_fonctions"])
 myRequest.ok_filenames_list.extend(["<++>"])
 myRequest.ok_filenames_list.extend(["<++>"])
 myRequest.ok_filenames_list.extend(["134_choses_finales"])
