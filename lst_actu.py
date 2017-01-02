9987c2cb6fea5b377c29857a269170e182b21832
Laurent Claessens <laurent@claessens-donadello.eu>
2017-01-02 15:01:02 +0100

    Un peu d'approximation de pi.

--- lst_actu.py
+++ lst_actu.py
@@ -17,8 +17,7 @@ myRequest.original_filename="mazhe.tex"
 
 myRequest.ok_filenames_list=["e_mazhe"]
 myRequest.ok_filenames_list.extend(["75_series_fonctions"])
-myRequest.ok_filenames_list.extend(["66_Chap_calcul_differentiel"])
-myRequest.ok_filenames_list.extend(["<++>"])
+myRequest.ok_filenames_list.extend(["174_series_fonctions"])
 myRequest.ok_filenames_list.extend(["<++>"])
 myRequest.ok_filenames_list.extend(["<++>"])
 myRequest.ok_filenames_list.extend(["<++>"])
