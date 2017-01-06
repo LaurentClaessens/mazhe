ef98df8c6ad8e0ccdf804d8bad2d224d408a7358
Laurent Claessens <laurent@claessens-donadello.eu>
2017-01-06 15:18:09 +0100

    Le théorème fondamental de l'analyse n'est pas une définition de l'intégrale.

--- lst_actu.py
+++ lst_actu.py
@@ -16,16 +16,7 @@ myRequest.add_plugin(plugins_agreg.set_isFrido,"before_pytex")
 myRequest.original_filename="mazhe.tex"
 
 myRequest.ok_filenames_list=["e_mazhe"]
-myRequest.ok_filenames_list.extend(["155_suites_series_fonctions"])
 myRequest.ok_filenames_list.extend(["71_suites_series_fonctions"])
-myRequest.ok_filenames_list.extend(["70_suites_series_fonctions"])
-myRequest.ok_filenames_list.extend(["169_suites_series_fonctions"])
-myRequest.ok_filenames_list.extend(["72_suites_series_fonctions"])
-myRequest.ok_filenames_list.extend(["155_suites_series_fonctions"])
-myRequest.ok_filenames_list.extend(["73_Chap_integrales_multiples"])
-myRequest.ok_filenames_list.extend(["38_Chap_integrales_multiples"])
-myRequest.ok_filenames_list.extend(["2_calcul_integral"])
-myRequest.ok_filenames_list.extend(["69_Chap_calcul_differentiel"])
 myRequest.ok_filenames_list.extend(["<++>"])
 myRequest.ok_filenames_list.extend(["<++>"])
 myRequest.ok_filenames_list.extend(["134_choses_finales"])
