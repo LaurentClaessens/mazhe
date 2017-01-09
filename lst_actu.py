2c569815a6924183bf3976014949049d4a827d51
Laurent Claessens <laurent@claessens-donadello.eu>
2017-01-09 04:42:33 +0100

    Inverse à gauche, inverse à droite.

--- lst_actu.py
+++ lst_actu.py
@@ -16,11 +16,8 @@ myRequest.add_plugin(plugins_agreg.set_isFrido,"before_pytex")
 myRequest.original_filename="mazhe.tex"
 
 myRequest.ok_filenames_list=["e_mazhe"]
-myRequest.ok_filenames_list.extend(["56_espace_vecto_norme"])
-myRequest.ok_filenames_list.extend(["174_series_fonctions"])
-myRequest.ok_filenames_list.extend(["<++>"])
-myRequest.ok_filenames_list.extend(["<++>"])
-myRequest.ok_filenames_list.extend(["<++>"])
+myRequest.ok_filenames_list.extend(["175_trigono"])
+myRequest.ok_filenames_list.extend(["55_espace_vecto_norme"])
 myRequest.ok_filenames_list.extend(["<++>"])
 myRequest.ok_filenames_list.extend(["<++>"])
 myRequest.ok_filenames_list.extend(["134_choses_finales"])
