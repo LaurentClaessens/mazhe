9b00c5818bde32752da5abacd54fd7db69033c91
Laurent Claessens <laurent@claessens-donadello.eu>
2017-01-22 20:03:12 +0100

    Méthode des caractéristiques pour les équations aux dérivées partielles d'ordre 1 et 2 dans R^2

--- lst_actu.py
+++ lst_actu.py
@@ -16,13 +16,11 @@ myRequest.add_plugin(plugins_agreg.set_isFrido,"before_pytex")
 myRequest.original_filename="mazhe.tex"
 
 myRequest.ok_filenames_list=["e_mazhe"]
+myRequest.ok_filenames_list.extend(["179_edp"])
+myRequest.ok_filenames_list.extend(["89_Equations_diff"])
 myRequest.ok_filenames_list.extend(["<++>"])
 myRequest.ok_filenames_list.extend(["<++>"])
 myRequest.ok_filenames_list.extend(["<++>"])
-myRequest.ok_filenames_list.extend(["<++>"])
-myRequest.ok_filenames_list.extend(["<++>"])
-myRequest.ok_filenames_list.extend(["161_EspacesProjectifs"])
-myRequest.ok_filenames_list.extend(["170_EspacesProjectifs"])
 myRequest.ok_filenames_list.extend(["134_choses_finales"])
 
 
