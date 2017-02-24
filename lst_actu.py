4de85472a0f352209bca42d745313db69f682df0
Laurent Claessens <laurent@claessens-donadello.eu>
2017-02-24 18:04:38 +0100

    Méthode des différences finies : début

--- lst_actu.py
+++ lst_actu.py
@@ -16,9 +16,10 @@ myRequest.add_plugin(plugins_agreg.set_isFrido,"before_pytex")
 myRequest.original_filename="mazhe.tex"
 
 myRequest.ok_filenames_list=["e_mazhe"]
-myRequest.ok_filenames_list.extend(["83_analyse_fonctionnelle"])
-myRequest.ok_filenames_list.extend(["179_edp"])
-myRequest.ok_filenames_list.extend(["150_ED_analyseCTU"])
+myRequest.ok_filenames_list.extend(["159_numerique"])
+myRequest.ok_filenames_list.extend(["<++>"])
+myRequest.ok_filenames_list.extend(["<++>"])
+myRequest.ok_filenames_list.extend(["<++>"])
 myRequest.ok_filenames_list.extend(["<++>"])
 myRequest.ok_filenames_list.extend(["134_choses_finales"])
 
