df954b8f75fb4ed735f2c9fb80d39a32d0a46147
Laurent Claessens <laurent@claessens-donadello.eu>
2017-02-24 13:30:46 +0100

    Utilisation des fonctions de Green pour résoudre une équation différentielle.

--- lst_actu.py
+++ lst_actu.py
@@ -17,8 +17,8 @@ myRequest.original_filename="mazhe.tex"
 
 myRequest.ok_filenames_list=["e_mazhe"]
 myRequest.ok_filenames_list.extend(["83_analyse_fonctionnelle"])
-myRequest.ok_filenames_list.extend(["<++>"])
-myRequest.ok_filenames_list.extend(["<++>"])
+myRequest.ok_filenames_list.extend(["179_edp"])
+myRequest.ok_filenames_list.extend(["150_ED_analyseCTU"])
 myRequest.ok_filenames_list.extend(["<++>"])
 myRequest.ok_filenames_list.extend(["134_choses_finales"])
 
