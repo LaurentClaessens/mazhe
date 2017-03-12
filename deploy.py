382f7bedabac0359228d8a99e06c8ca99da880a1
Laurent Claessens <laurent@claessens-donadello.eu>
2017-03-12 06:24:20 +0100

    deploy ... encore un nouveau projet pour améliorer mon quotidien.
    https://xkcd.com/1319/

---
+++ deploy.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python3
+# -*- coding: utf8 -*-
+
+# This script is intended to launch all tests and publish the new version of my texts.
+
+# - no future references in 'frido'
+# - no future references in 'mazhe'
+# - compile with no errors 'frido'
+# - compile with no errors 'mazhe'
+# - git status clean
+
+
+# If everything goes well :
+# - publish the results on my website.
