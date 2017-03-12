cfdb3aa857ffa1c1000b5b6e8887778c3eb3052f
Laurent Claessens <laurent@claessens-donadello.eu>
2017-03-12 08:00:30 +0100

    Les choses à faire dans deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,15 @@
 #! /bin/bash
 
 rm .deploy.log
+touch .deploy.log
+pytex lst_frido.py
+pytex lst_frido.py --verif
+pytex lst_everything.py
+pytex lst_everything.py --verif
+
+git status >> .deploy.log
+
+
+echo "Result : -----------"
+
+cat .deploy.log
