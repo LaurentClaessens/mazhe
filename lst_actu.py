d47542f636ac244e5cd78acb44d6cd6d4972a023
Laurent Claessens <laurent@claessens-donadello.eu>
2017-04-20 16:05:29 +0200

    Début vers les M-matrices.

--- lst_actu.py
+++ lst_actu.py
@@ -16,9 +16,7 @@ myRequest.add_plugin(plugins_agreg.set_isFrido,"before_pytex")
 myRequest.original_filename="mazhe.tex"
 
 myRequest.ok_filenames_list=["e_mazhe"]
-myRequest.ok_filenames_list.extend(["159_numerique"])
-myRequest.ok_filenames_list.extend(["<++>"])
-myRequest.ok_filenames_list.extend(["<++>"])
+myRequest.ok_filenames_list.extend(["60_EspacesVecto"])
 myRequest.ok_filenames_list.extend(["<++>"])
 myRequest.ok_filenames_list.extend(["<++>"])
 myRequest.ok_filenames_list.extend(["134_choses_finales"])
