446593a451f12cd06f84d1b16e58b7089a252982
Laurent Claessens <laurent@claessens-donadello.eu>
2017-04-30 05:54:46 +0200

    Fusion de deux définitions de la norme.

--- lst_actu.py
+++ lst_actu.py
@@ -16,11 +16,12 @@ myRequest.add_plugin(plugins_agreg.set_isFrido,"before_pytex")
 myRequest.original_filename="mazhe.tex"
 
 myRequest.ok_filenames_list=["e_mazhe"]
-myRequest.ok_filenames_list.extend(["60_EspacesVecto"])
-myRequest.ok_filenames_list.extend(["76_series_fonctions"])
+myRequest.ok_filenames_list.extend(["181_espace_vecto_norme"])
 myRequest.ok_filenames_list.extend(["<++>"])
 myRequest.ok_filenames_list.extend(["<++>"])
-myRequest.ok_filenames_list.extend(["56_espace_vecto_norme"])
+myRequest.ok_filenames_list.extend(["<++>"])
+myRequest.ok_filenames_list.extend(["<++>"])
+myRequest.ok_filenames_list.extend(["53_topologie"])
 myRequest.ok_filenames_list.extend(["134_choses_finales"])
 
 
