5557e9621652f3a523b05e979a5abd145efdc941
Laurent Claessens <laurent@claessens-donadello.eu>
2017-05-24 03:19:47 +0200

    Removing some files that will be recreated later (most of them).

--- lst_actu.py
+++ lst_actu.py
@@ -16,8 +16,7 @@ myRequest.add_plugin(plugins_agreg.set_isFrido,"before_pytex")
 myRequest.original_filename="mazhe.tex"
 
 myRequest.ok_filenames_list=["e_mazhe"]
-myRequest.ok_filenames_list.extend(["42_nombres"])
-myRequest.ok_filenames_list.extend(["46_groupes"])
+myRequest.ok_filenames_list.extend(["180_Equations_diff"])
 myRequest.ok_filenames_list.extend(["<++>"])
 myRequest.ok_filenames_list.extend(["<++>"])
 
