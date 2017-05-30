348b10920af89b071627eaa2f485587fd48804df
Laurent Claessens <laurent@claessens-donadello.eu>
2017-05-30 22:52:33 +0200

    Able to list all the URL that are in \url

--- testing/test_death_link.py
+++ testing/test_death_link.py
@@ -17,16 +17,37 @@ def exclude_dir(directory):
         return True
     return False
 
-def tex_file_iterator(directory):
+def _tex_file_iterator(directory):
     for p in os.listdir(directory):
         path=os.path.join(directory,p)
         if os.path.isfile(path):
             if path.endswith(".tex"):
                 yield path
         if os.path.isdir(path) and not exclude_dir(path):
-            for f in tex_file_iterator(path):
+            for f in _tex_file_iterator(path):
                 yield f
+
+def tex_file_iterator(directory):
+    """
+    Provides 'mazhe.bib' and then the '.tex' files in the
+    directory (recursive).
+    """
+    yield os.path.join(directory,"mazhe.bib")
+    for p in _tex_file_iterator(directory):
+        yield p
+
+def file_to_url_iterator(filename):
+    """
+    iterate over the url cited in 'filename'
+    """
+    pos=0
+    with open(filename,'r') as f:
+        text=f.read()
+
+    for line in text.split("\\url{")[1:]:
+        url=line[0:line.find("}")]
+        print(url)
 
 # Tel quel, il ne liste pas mazhe.bib
 for f in tex_file_iterator(starting_path):
-    print(f)
+    file_to_url_iterator(f)
