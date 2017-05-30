6de8acfbaec7bb6b1e60f84353d5c5fc336aa1ed
Lilian Besson <Naereen@users.noreply.github.com>
2017-05-31 01:29:37 +0200

    Better?

--- testing/test_death_link.py
+++ testing/test_death_link.py
@@ -1,15 +1,58 @@
 #! /usr/bin/python3
 # -*- coding: utf8 -*-
+r""" Read all the .tex files in the current directory, and check *every* link.
 
-# The directory passed as argument is the main directory of the
-# mazhe project.
-
+- Every broken link (in \url or \href) is displayed.
+- The directory passed as argument is the main directory of the mazhe project.
+"""
 
 import os
 import sys
 import string
 
-starting_path=os.path.abspath(sys.argv[1])
+try:
+    import requests
+
+    # use default solution with 
+    def is_not_dead(url):
+        try:
+            ret = requests.head(url)
+            return ret.status_code < 400
+        except Exception as e:
+            # print("Exception:", e)
+            # For debug, could be used to distinguish 404 errors
+            # and non-existing domains
+            return False
+# requests was not found, not an issue!
+except ImportError:
+    try:                 # assuming Python 3!
+        from http.client import HTTPConnection
+    except ImportError:  # using Python 2!
+        from httplib import HTTPConnection
+    try:                 # assuming Python 3!
+        from urllib.parse import urlparse
+    except ImportError:  # using Python 2!
+        from urlparse import urlparse
+    
+    # use other function, with HTTPConnection and urlparse
+    def is_not_dead(url):
+        try:
+            p = urlparse(url)
+            conn = HTTPConnection(p.netloc)
+            conn.request('HEAD', p.path)
+            resp = conn.getresponse()
+            return resp.status < 400
+        except Exception as e:
+            # print("Exception:", e)
+            # For debug, could be used to distinguish 404 errors
+            # and non-existing domains
+            return False
+
+
+starting_path = os.path.abspath(sys.argv[1])
+
+mainbib = "mazhe.bib"
+
 
 def exclude_dir(directory):
     if "build" in directory:
@@ -18,9 +61,10 @@ def exclude_dir(directory):
         return True
     return False
 
+
 def _tex_file_iterator(directory):
     for p in os.listdir(directory):
-        path=os.path.join(directory,p)
+        path = os.path.join(directory, p)
         if os.path.isfile(path):
             if path.endswith(".tex"):
                 yield path
@@ -28,12 +72,15 @@ def _tex_file_iterator(directory):
             for f in _tex_file_iterator(path):
                 yield f
 
+
 def tex_file_iterator(directory):
     """
     Provides 'mazhe.bib' and then the '.tex' files in the
     directory (recursive).
     """
-    yield os.path.join(directory,"mazhe.bib")
+    bib = os.path.join(directory, bib)
+    if os.path.exists(bib) and os.path.isfile(bib):
+        yield bib
     for p in _tex_file_iterator(directory):
         yield p
 
@@ -41,34 +88,35 @@ def _file_to_url_iterator(filename):
     """
     iterate over the url cited in 'filename'
     """
-    pos=0
-    with open(filename,'r') as f:
-        text=f.read()
+    pos = 0
+    # FIXME possible encoding issue!
+    with open(filename, 'r') as f:
+        text = f.read()
 
-    for line in text.split("\\url{")[1:]:
-        url=line[0:line.find("}")]
+    for line in text.split(r"\url{")[1:]:
+        url = line[0 : line.find("}")]
         yield url
-    for line in text.split("\\href{")[1:]:
-        url=line[0:line.find("}")]
+    for line in text.split(r"\href{")[1:]:
+        url = line[0 : line.find("}")]
         yield url
 
     # La frime serait d'utiliser des vues de listes
     # pour éviter la copie.
-    if filename.endswith("mazhe.bib"):
+    if filename.endswith(mainbib):
         for line in text.split("url =")[1:]:
-            start=line.find('"')
-            end=line.find('"',2)
-            url=line[start+1:end]
+            start = line.find('"')
+            end = line.find('"', 2)
+            url = line[start + 1 : end]
             if url is not "...":
                 yield url
 
 # List of death links that we don't care (because from other projects)
-useless_url=[]
+useless_url = []
 useless_url.append("http://xmaths.free.fr/1S/exos/1SstatexA2.pdf")
 useless_url.append("http://www.daniel-botton.fr/mathematiques/seconde/geometrie_plane/seconde_geometrie_plane_devoir.pdf")
 useless_url.append("<++>")
 useless_url.append("<++>")
-useless_url.append("<++>")
+useless_url.append("<++>")  # ? why different versions?
 
 def is_serious_url(url):
     if url == r"\lstname":
@@ -83,7 +131,6 @@ def file_to_url_iterator(filename):
         if is_serious_url(url):
             yield url
 
-            
 
 def check_url_corectness(url,f):
     if url=="":
@@ -92,14 +139,13 @@ def check_url_corectness(url,f):
         print("In ",f," : the url does not starts with an ascii character :")
         print(url)
 
-def is_death(url):
-    import requests
-    ret = requests.head(url)
-    return ret.status_code > 400
 
-for f in tex_file_iterator(starting_path):
-    for url in file_to_url_iterator(f):
-        check_url_corectness(url,f)
-        if is_death(url):
-            print("death link in ",f," :")
-            print(url)
+# Run script
+if __name__ == '__main__':
+    for f in tex_file_iterator(starting_path):
+        for url in file_to_url_iterator(f):
+            check_url_corectness(url,f)
+            if not is_death(url):
+                # FIXME we should print the line also
+                print("death link in ", f, " :")
+                print(url)
