78ba4fc9d77b83e16897c3c442b647984223b786
Laurent Claessens <laurent@claessens-donadello.eu>
2017-06-05 00:53:34 +0200

    Déplacer les nombres p-adiques vers les espaces vectoriels normés.

--- lst_actu.py
+++ lst_actu.py
@@ -16,10 +16,8 @@ myRequest.add_plugin(plugins_agreg.set_isFrido,"before_pytex")
 myRequest.original_filename="mazhe.tex"
 
 myRequest.ok_filenames_list=["e_mazhe"]
-myRequest.ok_filenames_list.extend(["79_inversion_locale"])
-myRequest.ok_filenames_list.extend(["60_EspacesVecto"])
-myRequest.ok_filenames_list.extend(["<++>"])
-myRequest.ok_filenames_list.extend(["<++>"])
+myRequest.ok_filenames_list.extend(["59_EspacesVecto"])
+myRequest.ok_filenames_list.extend(["181_espace_vecto_norme"])
 myRequest.ok_filenames_list.extend(["<++>"])
 myRequest.ok_filenames_list.extend(["<++>"])
 myRequest.ok_filenames_list.extend(["<++>"])
