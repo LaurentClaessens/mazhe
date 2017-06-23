f882ff721fad457e659629b2b63a0c0448e9f8e0
Laurent Claessens <laurent@claessens-donadello.eu>
2017-06-23 08:50:59 +0200

    Preparing a merge

--- lst_actu.py
+++ lst_actu.py
@@ -16,12 +16,10 @@ myRequest.add_plugin(plugins_agreg.set_isFrido,"before_pytex")
 myRequest.original_filename="mazhe.tex"
 
 myRequest.ok_filenames_list=["e_mazhe"]
-myRequest.ok_filenames_list.extend(["182_numerique"])
+myRequest.ok_filenames_list.extend(["79_inversion_locale"])
+myRequest.ok_filenames_list.extend(["68_Chap_calcul_differentiel"])
 myRequest.ok_filenames_list.extend(["<++>"])
-myRequest.ok_filenames_list.extend(["<++>"])
-myRequest.ok_filenames_list.extend(["<++>"])
-myRequest.ok_filenames_list.extend(["<++>"])
-myRequest.ok_filenames_list.extend(["<++>"])
+myRequest.ok_filenames_list.extend(["68_Chap_calcul_differentiel"])
 
 myRequest.ok_filenames_list.extend(["134_choses_finales"])
 myRequest.ok_filenames_list.extend(["157_thematique"])
