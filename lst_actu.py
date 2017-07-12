2a5929115eb42977fb5dc88d543bdde110d52365
Laurent Claessens <laurent@claessens-donadello.eu>
2017-07-12 10:01:08 +0200

    (organisation) Mettre un peu d'ordre dans le chapitre sur l'analyse réelle.
    - Fonctions de deux variables après celles de une variable
    - Fusion des parties sur les dérivées directionnelles

--- lst_actu.py
+++ lst_actu.py
@@ -16,10 +16,13 @@ myRequest.add_plugin(plugins_agreg.set_isFrido,"before_pytex")
 myRequest.original_filename="mazhe.tex"
 
 myRequest.ok_filenames_list=["e_mazhe"]
-myRequest.ok_filenames_list.extend(["introAgreg"])
-myRequest.ok_filenames_list.extend(["sage"])
-myRequest.ok_filenames_list.extend(["<++>"])
-myRequest.ok_filenames_list.extend(["<++>"])
+myRequest.ok_filenames_list.extend(["65_analyseR"])
+myRequest.ok_filenames_list.extend(["67_analyseR"])
+myRequest.ok_filenames_list.extend(["64_analyseR"])
+myRequest.ok_filenames_list.extend(["66_analyseR"])
+myRequest.ok_filenames_list.extend(["173_analyseR"])
+myRequest.ok_filenames_list.extend(["68_analyseR"])
+myRequest.ok_filenames_list.extend(["69_analyseR"])
 myRequest.ok_filenames_list.extend(["<++>"])
 myRequest.ok_filenames_list.extend(["<++>"])
 
