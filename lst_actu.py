4422af5fa726c44068d63bb0b5a69ac3c13739dc
Laurent Claessens <laurent@claessens-donadello.eu>
2017-07-28 22:40:53 +0200

    (groupes et représentations) Groupe des isométries du tétraèdre et représentations de S_4.

--- lst_actu.py
+++ lst_actu.py
@@ -16,9 +16,10 @@ myRequest.add_plugin(plugins_agreg.set_isFrido,"before_pytex")
 myRequest.original_filename="mazhe.tex"
 
 myRequest.ok_filenames_list=["e_mazhe"]
-myRequest.ok_filenames_list.extend(["140_EspacesAffines"])
 myRequest.ok_filenames_list.extend(["63_representations"])
-myRequest.ok_filenames_list.extend(["175_trigono"])
+myRequest.ok_filenames_list.extend(["62_representations"])
+myRequest.ok_filenames_list.extend(["47_groupes"])
+myRequest.ok_filenames_list.extend(["<++>"])
 myRequest.ok_filenames_list.extend(["<++>"])
 myRequest.ok_filenames_list.extend(["<++>"])
 myRequest.ok_filenames_list.extend(["<++>"])
