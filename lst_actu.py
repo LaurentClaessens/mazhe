00f3b159ffb222b52bef32efa9a66cd7e74971a9
Laurent Claessens <laurent@claessens-donadello.eu>
2017-08-01 03:37:55 +0200

    (réorganisation) Beaucoup de déplacements et de découpages de chapitres pour respecter les références vers le futur.
    Entre autres choses :
    
    - théorie des groupes découpée
    - espaces vectoriels normés découpés
    - connexité de certains groupes au bout de l'analyse réelle

--- lst_actu.py
+++ lst_actu.py
@@ -12,20 +12,34 @@ myRequest = latexparser.PytexTools.Request()
 myRequest.ok_hash=commons.ok_hash
 
 myRequest.add_plugin(plugins_agreg.set_isFrido,"before_pytex")
-
 myRequest.original_filename="mazhe.tex"
-
 myRequest.ok_filenames_list=["e_mazhe"]
 
-myRequest.ok_filenames_list.append("47_groupes.tex") 
+
+myRequest.ok_filenames_list.extend(["<++>"])
+myRequest.ok_filenames_list.extend(["<++>"])
+myRequest.ok_filenames_list.extend(["<++>"])
+
+myRequest.ok_filenames_list.append("61_representations.tex") 
+myRequest.ok_filenames_list.append("64_analyseR.tex") 
+myRequest.ok_filenames_list.append("61_representations.tex") 
+myRequest.ok_filenames_list.append("64_analyseR.tex") 
+myRequest.ok_filenames_list.append("61_representations.tex") 
+myRequest.ok_filenames_list.append("67_analyseR.tex") 
+myRequest.ok_filenames_list.append("61_representations.tex") 
+myRequest.ok_filenames_list.append("181_espace_vecto_norme.tex") 
+myRequest.ok_filenames_list.append("61_representations.tex") 
+myRequest.ok_filenames_list.append("181_espace_vecto_norme.tex") 
+myRequest.ok_filenames_list.append("140_EspacesAffines.tex") 
 myRequest.ok_filenames_list.append("175_trigono.tex") 
-
-
-myRequest.ok_filenames_list.extend(["<++>"])
-myRequest.ok_filenames_list.extend(["<++>"])
-myRequest.ok_filenames_list.extend(["<++>"])
-myRequest.ok_filenames_list.extend(["<++>"])
-myRequest.ok_filenames_list.extend(["<++>"])
+myRequest.ok_filenames_list.append("61_representations.tex") 
+myRequest.ok_filenames_list.append("140_EspacesAffines.tex") 
+myRequest.ok_filenames_list.append("153_representations.tex") 
+myRequest.ok_filenames_list.append("140_EspacesAffines.tex") 
+myRequest.ok_filenames_list.append("61_representations.tex") 
+myRequest.ok_filenames_list.append("181_espace_vecto_norme.tex") 
+myRequest.ok_filenames_list.append("61_representations.tex") 
+myRequest.ok_filenames_list.append("181_espace_vecto_norme.tex") 
 
 myRequest.ok_filenames_list.extend(["134_choses_finales"])
 myRequest.ok_filenames_list.extend(["157_thematique"])
