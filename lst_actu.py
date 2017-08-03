670c17847e88885afa4fe4207db227eebf500490
Laurent Claessens <laurent@claessens-donadello.eu>
2017-08-03 06:41:53 +0200

    (groupe symétrique) liste des sous-groupes normaux de S_n.

--- lst_actu.py
+++ lst_actu.py
@@ -16,30 +16,9 @@ myRequest.original_filename="mazhe.tex"
 myRequest.ok_filenames_list=["e_mazhe"]
 
 
+myRequest.ok_filenames_list.extend(["186_groupes"])
 myRequest.ok_filenames_list.extend(["<++>"])
 myRequest.ok_filenames_list.extend(["<++>"])
-myRequest.ok_filenames_list.extend(["<++>"])
-
-myRequest.ok_filenames_list.append("61_representations.tex") 
-myRequest.ok_filenames_list.append("64_analyseR.tex") 
-myRequest.ok_filenames_list.append("61_representations.tex") 
-myRequest.ok_filenames_list.append("64_analyseR.tex") 
-myRequest.ok_filenames_list.append("61_representations.tex") 
-myRequest.ok_filenames_list.append("67_analyseR.tex") 
-myRequest.ok_filenames_list.append("61_representations.tex") 
-myRequest.ok_filenames_list.append("181_espace_vecto_norme.tex") 
-myRequest.ok_filenames_list.append("61_representations.tex") 
-myRequest.ok_filenames_list.append("181_espace_vecto_norme.tex") 
-myRequest.ok_filenames_list.append("140_EspacesAffines.tex") 
-myRequest.ok_filenames_list.append("175_trigono.tex") 
-myRequest.ok_filenames_list.append("61_representations.tex") 
-myRequest.ok_filenames_list.append("140_EspacesAffines.tex") 
-myRequest.ok_filenames_list.append("153_representations.tex") 
-myRequest.ok_filenames_list.append("140_EspacesAffines.tex") 
-myRequest.ok_filenames_list.append("61_representations.tex") 
-myRequest.ok_filenames_list.append("181_espace_vecto_norme.tex") 
-myRequest.ok_filenames_list.append("61_representations.tex") 
-myRequest.ok_filenames_list.append("181_espace_vecto_norme.tex") 
 
 myRequest.ok_filenames_list.extend(["134_choses_finales"])
 myRequest.ok_filenames_list.extend(["157_thematique"])
