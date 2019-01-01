4dc2d24c309c88fc4dd24dce4f5732ac6f69a548
Laurent Claessens <laurent@claessens-donadello.eu>
2019-01-01 07:26:33 +0100

    (giulietta) Rename 'everything' -> 'giulietta'

--- lst_giulietta.py
+++ lst_giulietta.py
@@ -12,10 +12,10 @@ myRequest.ok_hash=commons.ok_hash
 # alors que keep_script_marks compte dessus pour faire sa sélection.
 myRequest.add_plugin(PytexTools.accept_all_input,"medicament")
 myRequest.add_plugin(PytexTools.keep_script_marks(plugins_agreg.mazhe_mark_list),"before_pytex")
-myRequest.add_plugin(plugins_agreg.set_boolean("isEverything","true"),"before_pytex")
+myRequest.add_plugin(plugins_agreg.set_boolean("isGiulietta","true"),"before_pytex")
 myRequest.add_plugin(plugins_agreg.set_commit_hexsha,"after_pytex")
 
-myRequest.new_output_filename="0-everything.pdf"
+myRequest.new_output_filename="0-giulietta.pdf"
 
 
 myRequest.original_filename="mazhe.tex"
