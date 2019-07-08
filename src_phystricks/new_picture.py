0702403df0d3671fabd435520743d360e2f3e900
Laurent Claessens <laurent@claessens-donadello.eu>
2019-07-08 21:53:55 +0200

    (linter) Autopep8 sur `new_picture.py`.

--- src_phystricks/new_picture.py
+++ src_phystricks/new_picture.py
@@ -7,26 +7,28 @@ import string
 
 from src.NoMathUtilities import SubdirectoryFilenames
 
-def via_random(alphabet,n):
-    a=""
-    for k in range(0,n):
-        a=a+random.choice(alphabet)
+
+def via_random(alphabet, n):
+    a = ""
+    for k in range(0, n):
+        a = a+random.choice(alphabet)
     return a
 
+
 try:
-    figure_name=sys.argv[1]
+    figure_name = sys.argv[1]
 except IndexError:
-    a=via_random(string.ascii_uppercase,4)
-    b=via_random(string.ascii_letters,8)
-    figure_name=a+"oo"+b
+    a = via_random(string.ascii_uppercase, 4)
+    b = via_random(string.ascii_letters, 8)
+    figure_name = a+"oo"+b
 
-forbidden_symb=["_","1","2","3","4","5","6","7","8","9","0"]
+forbidden_symb = ["_", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
 
-for s in forbidden_symb :
+for s in forbidden_symb:
     if s in figure_name:
         raise ValueError("You should not use '{0}' in the name.".format(s))
 
-code_base="""# -*- coding: utf8 -*-
+code_base = """# -*- coding: utf8 -*-
 from phystricks import *
 def XXXX():
     pspict,fig = SinglePicture("XXXX")
@@ -80,25 +82,32 @@ def XXXX():
 
 """
 
-def create_file(sfile,text):
+
+def create_file(sfile, text):
     if not os.path.isfile(sfile.abspath()):
-        with open(sfile.abspath(),"w") as f:
+        with open(sfile.abspath(), "w") as f:
             f.write(text)
-    else :
+    else:
         print("Le fichier {} existe déjà. Je ne fais rien".format(filename))
 
-code=code_base.replace("XXXX",figure_name)
 
-filename=SubdirectoryFilenames("phystricks%s.py"%figure_name,"pictures_src")
-pstricksfilename=SubdirectoryFilenames("Fig_{}.pstricks".format(figure_name),"pictures_tex")
-pdffilename=SubdirectoryFilenames("tikzFIGLabelFig"+figure_name+"PICT"+figure_name+".pdf","pictures_tikz")
-md5filename=SubdirectoryFilenames("tikzFIGLabelFig"+figure_name+"PICT"+figure_name+".md5","pictures_tikz")
+code = code_base.replace("XXXX", figure_name)
+
+filename = SubdirectoryFilenames(
+    "phystricks%s.py" % figure_name, "pictures_src")
+pstricksfilename = SubdirectoryFilenames(
+    "Fig_{}.pstricks".format(figure_name), "pictures_tex")
+pdffilename = SubdirectoryFilenames(
+    "tikzFIGLabelFig"+figure_name+"PICT"+figure_name+".pdf", "pictures_tikz")
+md5filename = SubdirectoryFilenames(
+    "tikzFIGLabelFig"+figure_name+"PICT"+figure_name+".md5", "pictures_tikz")
 
 
-for f in [filename,pstricksfilename,pdffilename]:
-    create_file(f,code)
-create_file(md5filename,"")
+for f in [filename, pstricksfilename, pdffilename]:
+    create_file(f, code)
+create_file(md5filename, "")
 
-print("from phystricks{} import {}".format(figure_name,figure_name))
-print("git add {} {} {} {}".format(filename.from_here(),pstricksfilename.from_here(),pdffilename.from_here(),md5filename.from_here()))
-print("attach('{}');{}();exit()".format(filename.filename,figure_name)   )
+print("from phystricks{} import {}".format(figure_name, figure_name))
+print("git add {} {} {} {}".format(filename.from_here(),
+                                   pstricksfilename.from_here(), pdffilename.from_here(), md5filename.from_here()))
+print("attach('{}');{}();exit()".format(filename.filename, figure_name))
