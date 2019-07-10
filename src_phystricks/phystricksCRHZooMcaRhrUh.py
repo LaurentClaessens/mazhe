4e95650873dfd10a90113c62964bc23fc2cad737
Laurent Claessens <laurent@claessens-donadello.eu>
2019-07-10 06:02:35 +0200

    (pavage) fGf^-1 est aussi un groupe de pavage.

--- src_phystricks/phystricksCRHZooMcaRhrUh.py
+++ src_phystricks/phystricksCRHZooMcaRhrUh.py
@@ -2,6 +2,8 @@
 
 from __future__ import division
 
+import itertools
+
 from phystricks import *
 
 
@@ -17,20 +19,44 @@ def action(k, l, m, P):
     return new_P
 
 
+def move_polygon(k, l, m, polygon):
+    """Move the polygon with the group."""
+    new_vertices = [action(k, l, m, P) for P in polygon.vertices]
+    return Polygon(new_vertices)
+
+
 def CRHZooMcaRhrUh():
-    pspict,fig = SinglePicture("CRHZooMcaRhrUh")
-    #pspict.dilatation_X(1)
-    #pspict.dilatation_Y(1)
+    pspict, fig = SinglePicture("CRHZooMcaRhrUh")
+    # pspict.dilatation_X(1)
+    # pspict.dilatation_Y(1)
     pspict.dilatation(1)
 
-    A = Point(0,0)
+    A = Point(0, 0)
     B = Point(1/2, -sqrt(3)/6)
-    C = Point(1,0)
+    C = Point(1, 0)
     D = Point(1/2, sqrt(3)/6)
 
-    base = Polygon(A,B,C,D)
+    base = Polygon(A, B, C, D)
 
-    pspict.DrawGraphs(base)
+    rotated_polys = [move_polygon(0, 0, k, base) for k in [0, 1, 2]]
+    
+    max_translate = 3
+    K = range(-max_translate, max_translate + 1)
+    L = range(-max_translate, max_translate + 1)
+    M = [0, 1, 2]
+
+    print(action(0,0,1,Point(1,0)))
+    print(action(0,0,2,Point(1,0)))
+    print(action(0,0,3,Point(1,0)))
+    print(action(0,0,4,Point(1,0)))
+
+    polys = []
+    for k,l,m in itertools.product(K, L, M):
+        polys.append(move_polygon(k,l,m,base))
+        
+
+    pspict.DrawGraphs(polys)
+
     pspict.DrawDefaultAxes()
     fig.no_figure()
     fig.conclude()
