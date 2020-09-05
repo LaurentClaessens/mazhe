d1f3da6a62ce1958568360b94b72b13bdf5f9ead
Laurent Claessens <laurent@claessens-donadello.eu>
2020-09-05 08:05:03 +0200

    (linter)

--- lst_book.py
+++ lst_book.py
@@ -1,17 +1,17 @@
-# -*- coding: utf8 -*-
-
 from pytex.src import PytexTools
 import commons
 import plugins_agreg
 
 myRequest = PytexTools.Request("mesure")
-myRequest.ok_hash=commons.ok_hash
-myRequest.original_filename="mazhe.tex"
+myRequest.ok_hash = commons.ok_hash
+myRequest.original_filename = "mazhe.tex"
 
-# L'ordre dans les plugin est important parce que set_isFrido retourne un code latex sans les commentaires
+# L'ordre dans les plugin est important parce que set_isFrido retourne
+# un code latex sans les commentaires
 # alors que keep_script_marks compte dessus pour faire sa sélection.
 myRequest.add_plugin(PytexTools.accept_all_input, "options")
-myRequest.add_plugin(PytexTools.keep_script_marks(plugins_agreg.frido_mark_list),"before_pytex")
+plugin = PytexTools.keep_script_marks(plugins_agreg.frido_mark_list)
+myRequest.add_plugin(plugin, "before_pytex")
 
 
 # the plugin "split_doc" should better be of type "medicament"
@@ -19,12 +19,13 @@ myRequest.add_plugin(PytexTools.keep_script_marks(plugins_agreg.frido_mark_list)
 # by himself instead of hard-code it in the function.
 
 # If you change the '4' here, you have to change it also in 'split_book.py'
-myRequest.add_plugin(plugins_agreg.split_toc("book",4),
-                                             "before_compilation")
+myRequest.add_plugin(plugins_agreg.split_toc("book", 4), "before_compilation")
 
-myRequest.add_plugin(plugins_agreg.set_boolean("isBook","true"),"before_pytex")
-myRequest.add_plugin(plugins_agreg.set_pdftitle("Le Frido"),"before_pytex")
-myRequest.add_plugin(plugins_agreg.set_commit_hexsha,"after_pytex")
-myRequest.add_plugin(plugins_agreg.assert_MonCerveau_first,"after_compilation")
+myRequest.add_plugin(plugins_agreg.set_boolean("isBook", "true"),
+                     "before_pytex")
+myRequest.add_plugin(plugins_agreg.set_pdftitle("Le Frido"), "before_pytex")
+myRequest.add_plugin(plugins_agreg.set_commit_hexsha, "after_pytex")
+myRequest.add_plugin(plugins_agreg.assert_MonCerveau_first,
+                     "after_compilation")
 
-myRequest.new_output_filename="0-book.pdf"
+myRequest.new_output_filename = "0-book.pdf"
