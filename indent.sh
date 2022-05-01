f42cef986b13f1ff89beab27c9c34235064c8d4a
Laurent Claessens <laurent@claessens-donadello.eu>
2022-05-01 07:29:29 +0200

    (indent)

--- indent.sh
+++ indent.sh
@@ -4,7 +4,7 @@ base_dir=$(pwd)
 frido_dir=$base_dir/tex/frido
 
 cd $frido_dir
-for f in (ls *.tex); echo $f ; latexindent -w  $f ;end
+for f in $(ls *.tex); do echo $f ; latexindent -w  $f ;done
 
 
 cd $base_dir
