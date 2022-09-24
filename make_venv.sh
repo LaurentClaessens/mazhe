f99ade659875ac9231252055b502d0e1999b9cf6
Laurent Claessens <laurent@claessens-donadello.eu>
2022-09-24 07:20:33 +0200

    (pytex) Create the venv.

--- make_venv.sh
+++ make_venv.sh
@@ -1,53 +1,47 @@
 #!/bin/bash
 
-#
-# This script installs the required venv for the linter.
-#
-
 # apt install  make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl git
-
-
-# git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-# cd ~/.pyenv/libexec
-# ./pyenv install 3.7.4
 
 # You may have to hack
 # cd /usr/lib/x86_64-linux-gnu
 # sudo ln -s libffi.so.7 libffi.so.6
 
-VERSION=3.8.3
-PYTHON3=~/.pyenv/versions/$VERSION/bin/python3
-MAIN_DIR=$PWD
-VENV_DIR=$MAIN_DIR/venv
-BIN_DIR=$VENV_DIR/bin
+#
+# git clone https://github.com/pyenv/pyenv.git
+# cd .pyenv/bin
+# ./pyenv install 3.10.4
+
+MAIN_DIR=$(pwd)
+VENV_DIR="$MAIN_DIR/venv"
+BIN_DIR="$VENV_DIR/bin"
+PYTHON_VERSION=3.10.4
+
+PYTHON3="$HOME/.pyenv/versions/$PYTHON_VERSION/bin/python3"
+
+echo "le python3 sera $PYTHON3"
 
 
-pip3_install()
+function install_pytex()
 {
-    PACKAGE=$1
-    cd $BIN_DIR
-    ./pip3 install $PACKAGE
+    echo "bonjour"
+    pak_dir=$VENV_DIR/lib/python3.10/site-packages
+    pytex_dir=$pak_dir/pytex
+    if [ ! -d "$DIRECTORY" ]; then
+      echo "$pytex_dir does not exist."
+      cd $pak_dir
+      git clone git@github.com:LaurentClaessens/pytex.git
+    fi
 }
 
-upgrade_pip()
-{
-    cd $BIN_DIR
-    ./pip3 install --upgrade pip
-}
+echo "Creating the virtual environment"
+"$PYTHON3" -m venv "$VENV_DIR"
 
-install_venv()
-{
-    echo $PYTHON3
-    echo $VENV_DIR
-    $PYTHON3 -m venv $VENV_DIR
-}
 
-install_pip_packages()
-{
-    # pass
-    echo "rien à installer ?"
-}
+cd "$BIN_DIR" || exit 1
+./pip3 install --upgrade pip
 
-install_venv
-upgrade_pip
-install_pip_packages
+cd "$BIN_DIR" || exit 1
+./pip3 install -r "$MAIN_DIR/requirements.txt"
+
+
+install_pytex
