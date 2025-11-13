fac2f509a58c925123dd6b9b9bedce8937b3be4d
quark67 <quark67@free.fr>
2025-11-13 03:51:46 +0100

    Update code_sage2.py (erreur de syntaxe depuis que Sage emploie Python3)
    Les print nécessitent des parenthèse. L'erreur est : SyntaxError: Missing parentheses in call to 'print'. Did you mean print(...)?

--- tex/frido/code_sage2.py
+++ tex/frido/code_sage2.py
@@ -1,11 +1,11 @@
 from scipy import stats
 
 N=stats.norm
-print N.mean()      # 0
-print N.var()       # 1
+print(N.mean())      # 0
+print(N.var())       # 1
 
 xi = N.isf(0.95)
-print xi            # -1.64485
+print(xi)            # -1.64485
 
 N.cdf(xi)           # Vérification : 0.05
 
